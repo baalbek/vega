0a8fefd16f40ade7414366c9a1f131b2088e6f16
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2013-06-05 22:06:20 +0200

    Success moved from maunaloa and test

---
+++ src/test/java/vega/mocks/DerivativeBean.java
@@ -0,0 +1,160 @@
+package vega.mocks;
+
+import oahu.financial.Derivative;
+import oahu.financial.OptionCalculator;
+import oahu.financial.Stock;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rcs
+ * Date: 5/1/13
+ * Time: 9:35 AM
+ */
+public class DerivativeBean implements Derivative {
+    private final OptionCalculator calculator;
+
+    Stock parent;
+
+    public DerivativeBean(String ticker,
+                                    int opType,
+                                    double x,
+                                    double buy,
+                                    double sell,
+                                    double days,
+                                    Stock parent,
+                                    OptionCalculator calculator) {
+        this.ticker = ticker;
+        this.opType = opType;
+        this.x = x;
+        this.buy = buy;
+        this.sell = sell;
+        this.days = days;
+        this.parent = parent;
+        this.calculator = calculator;
+    }
+
+    public Stock getParent() {
+        return parent;
+    }
+
+    //--------------------------------------------------
+    //------------- Expiry
+    //--------------------------------------------------
+    private Date expiry;
+    public Date getExpiry() {
+        return expiry;
+    }
+    public void setExpiry(Date value) {
+        expiry = value;
+    }
+
+    //--------------------------------------------------
+    //------------- OpType
+    //--------------------------------------------------
+    private int opType;
+    @Override
+    public int getOpType() {
+        return opType;
+    }
+    public void setOpType(int value) {
+        opType = value;
+    }
+
+    //--------------------------------------------------
+    //------------- X
+    //--------------------------------------------------
+    double x;
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double value) {
+        x = value;
+    }
+
+    //--------------------------------------------------
+    //------------- Buy
+    //--------------------------------------------------
+    double buy;
+    @Override
+    public double getBuy() {
+        return buy;
+    }
+    public void setBuy(double value) {
+        buy = value;
+    }
+
+    //--------------------------------------------------
+    //------------- Sell
+    //--------------------------------------------------
+    double sell;
+    @Override
+    public double getSell() {
+        return sell;
+    }
+    public void setSell(double value) {
+        sell = value;
+    }
+
+    //--------------------------------------------------
+    //------------- Ticker
+    //--------------------------------------------------
+    String ticker;
+    public String getTicker() {
+        return ticker;
+    }
+    public void setTicker(String value) {
+        ticker = value;
+    }
+
+    //--------------------------------------------------
+    //------------- days
+    //--------------------------------------------------
+    double days;
+    public double getDays() {
+        return days;
+    }
+    public void setDays(double value) {
+        days = value;
+    }
+    public double getYears() {
+        return getDays() / 365.0;
+    }
+    //--------------------------------------------------
+    //------------- ivBuy
+    //--------------------------------------------------
+
+    public double getIvBuy() {
+        return calculator.iv(this,Derivative.BUY);
+    }
+    //--------------------------------------------------
+    //------------- ivSell
+    //--------------------------------------------------
+    public double getIvSell() {
+        return calculator.iv(this,Derivative.SELL);
+    }
+    //--------------------------------------------------
+    //------------- Delta
+    //--------------------------------------------------
+    public double getDelta() {
+        return calculator.delta(this);
+    }
+
+    //--------------------------------------------------
+    //------------- Break-even
+    //--------------------------------------------------
+    public double getBreakeven() {
+        return calculator.breakEven(this);
+    }
+
+    //--------------------------------------------------
+    //------------- Spread
+    //--------------------------------------------------
+    public double getSpread() {
+        return calculator.spread(this);
+    }
+
+}
---
+++ src/test/java/vega/mocks/StockBean.java
@@ -0,0 +1,114 @@
+package vega.mocks;
+
+import oahu.financial.Stock;
+
+import java.util.Date;
+import org.joda.time.DateMidnight;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rcs
+ * Date: 11/21/12
+ * Time: 10:43 PM
+ */
+public class StockBean implements Stock {
+
+    private int id;
+    private int tickerId;
+
+    public StockBean() {
+    }
+
+    public StockBean(Date dx,
+                     double opn,
+                     double hi,
+                     double lo,
+                     double cls,
+                     int volume) {
+        setDx(dx);
+        setOpn(opn);
+        setHi(hi);
+        setLo(lo);
+        setCls(cls);
+        setVolume(volume);
+
+    }
+    //region Time
+    //private Date dx = null;
+    private DateMidnight dxJoda;
+
+    public Date getDx() {
+        return dxJoda.toDate();
+    }
+
+    public void setDx(Date value) {
+        dxJoda = new DateMidnight(value);
+    }
+
+
+    public DateMidnight getDxJoda() {
+        return dxJoda;
+    }
+
+    public void setDxJoda(DateMidnight value) {
+        this.dxJoda = value;
+    }
+    //endregion Time
+
+
+    double opn;
+    public double getOpn() {
+        return opn;
+    }
+    public void setOpn(double value) {
+        opn = value;
+    }
+
+    double hi;
+    public double getHi() {
+        return hi;
+    }
+    public void setHi(double value) {
+        hi = value;
+    }
+
+    double lo;
+    public double getLo() {
+        return lo;
+    }
+    public void setLo(double value) {
+        lo = value;
+    }
+
+    double cls;
+    public double getCls() {
+        return cls;
+    }
+    public void setCls(double value) {
+        cls = value;
+    }
+
+    int volume;
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int value) {
+        volume = value;
+    }
+
+    public double getValue() {
+        /*
+        if (value == null) {
+            value = new SimpleDoubleProperty(getCls());
+        }
+        return value.get();
+        */
+        return cls;
+    }
+
+
+    public String getTicker() {
+        return "TEST";
+    }
+
+}
